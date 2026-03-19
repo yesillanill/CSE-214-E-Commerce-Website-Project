7690bb3601cf37f9c7bc89f21b454e18d43039bd
Anıl Yeşil <yesillanill@outlook.com>
2026-03-19 15:26:32 +0300

    19.03.2026 15.26

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,18 +1,27 @@
 import { AppService } from './core/services/app.service';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { Sidebar } from './components/layout/sidebar/sidebar';
+import { AuthService } from './core/services/auth.service';
+import { CartService } from './core/services/cart.service';
+import { WishlistService } from './core/services/wishlist.service';
+import { CommonModule, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, RouterLink, Sidebar, CommonModule, DecimalPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 
 export class App {
-  constructor(private appService : AppService){
+  constructor(
+    private appService: AppService,
+    public auth: AuthService,
+    public cartService: CartService,
+    public wishlistService: WishlistService
+  ){
     this.appService.initApp();
   }
 }
